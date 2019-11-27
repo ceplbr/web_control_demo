ef8b4cbb302c88ec2952146c82117a57efba5208
Miroslav Cepl <cepl@benderrobotics.com>
2019-11-27 20:27:13 +0100

    Added correct local and global path handling

--- control/static/js/control.js
+++ control/static/js/control.js
@@ -1,4 +1,97 @@
-  /**
+ 
+  GlobalPathHandler = function(options) {
+    var that = this;
+    options = options || {};
+    var ros = options.ros;
+
+    var topic = options.topic || '/move_base/NavfnROS/plan';
+    this.rootObject = options.rootObject || new createjs.Container();
+    var continuous = options.continuous;
+    var strokeColor = options.strokeColor || createjs.Graphics.getRGB(0, 0, 0);
+    var strokeSize = options.strokeSize || 3;
+  
+    this.currentGlobalPath = new ROS2D.PathShape();
+    this.rootObject.addChild(this.currentGlobalPath);
+    
+    var rosTopic = new ROSLIB.Topic({
+      ros : ros,
+      name : topic,
+      messageType : 'nav_msgs/Path'
+    });
+
+    rosTopic.subscribe(function(message) {
+      var index = null;
+      if (that.currentGlobalPath) {
+        index = that.rootObject.getChildIndex(that.currentGlobalPath);
+        that.rootObject.removeChild(that.currentGlobalPath);
+      }
+
+      that.currentGlobalPath = new ROS2D.PathShape({
+        ros: ros,
+        path: message,
+        rootObject : that.rootObject,
+        strokeSize : strokeSize,
+        strokeColor : strokeColor,
+      });
+
+      if (index !== null) {
+        that.rootObject.addChildAt(that.currentGlobalPath, index);
+      }
+      else {
+        that.rootObject.addChild(that.currentGlobalPath);
+      }
+
+    });
+  };
+  GlobalPathHandler.prototype.__proto__ = EventEmitter2.prototype;
+
+   
+LocalPathHandler = function(options) {
+    var that = this;
+    options = options || {};
+    var ros = options.ros;
+    var topic = options.topic || '/move_base/NavfnROS/plan';
+    this.rootObject = options.rootObject || new createjs.Container();
+    var continuous = options.continuous;
+    var strokeColor = options.strokeColor || createjs.Graphics.getRGB(0, 0, 0);
+    var strokeSize = options.strokeSize || 3;
+  
+    this.currentLocalPath = new ROS2D.PathShape();
+    this.rootObject.addChild(this.currentLocalPath);
+
+    var rosTopic = new ROSLIB.Topic({
+      ros : ros,
+      name : topic,
+      messageType : 'nav_msgs/Path'
+    });
+
+    rosTopic.subscribe(function(message) {
+      var index = null;
+      if (that.currentLocalPath) {
+        index = that.rootObject.getChildIndex(that.currentLocalPath);
+        that.rootObject.removeChild(that.currentLocalPath);
+      }
+
+      that.currentLocalPath = new ROS2D.PathShape({
+        ros: ros,
+        path: message,
+        rootObject : that.rootObject,
+        strokeSize : strokeSize,
+        strokeColor : strokeColor
+      });
+
+      if (index !== null) {
+        that.rootObject.addChildAt(that.currentLocalPath, index);
+      }
+      else {
+        that.rootObject.addChild(that.currentLocalPath);
+      }
+
+    });
+ };
+LocalPathHandler.prototype.__proto__ = EventEmitter2.prototype;
+
+/**
    * Setup all GUI elements when the page is loaded.
    */
 function init() {
@@ -25,26 +118,25 @@ function init() {
         withOrientation: 'True'
     });
 
-    path = new ROSLIB.Topic({
-        ros: ros,
-        name: '/move_base/NavfnROS/plan',
-        messageType: 'nav_msgs/Path',
-        rootObject : viewer.scene
+    var global_plan_color = createjs.Graphics.getRGB(0, 255, 0);
+    var local_plan_color = createjs.Graphics.getRGB(255, 0, 0);
+
+    /* Setup path handlers */
+    var global_path = GlobalPathHandler({
+        ros : ros,
+        rootObject : viewer.scene,
+        strokeColor : global_plan_color,
+        strokeSize : 0.02
     });
 
-    path.subscribe(function (message) {
-        //alert('Received message on ' + path.name + ': ' + JSON.stringify(message));
-        targetPath = new ROS2D.PathShape({
-            ros: ros,
-            path: message,
-            rootObject : viewer.scene,
-            strokeSize : 0.01
-        });
-        viewer.scene.addChild(targetPath);
-        targetPath.setPath(message);
-
-        //path.unsubscribe();
+    var local_path = LocalPathHandler({
+        ros : ros,
+        rootObject : viewer.scene,
+        topic : '/move_base/TebLocalPlannerROS/local_plan',
+        strokeColor : local_plan_color,
+        strokeSize : 0.02
     });
+    
     var target_iframe = '<iframe class="videoframe" src="http://' + target_ip + ':8282/stream?topic=/cv_camera/image_raw"></iframe>';
     document.getElementById("videofeed").innerHTML=target_iframe;
 }
